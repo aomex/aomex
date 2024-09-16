f0942cdad3b4b21a07c8000598587f3a26d68115
geekact <geekacting@gmail.com>
2024-09-16 11:34:53 +0800

    refactor(internal-tools): sleep使用timers内置的setTimeout

--- packages/internal-tools/src/sleep.ts
+++ packages/internal-tools/src/sleep.ts
@@ -1,9 +1,8 @@
+import timers from 'node:timers/promises';
+
 /**
  * 等待指定的时间（毫秒）后再往下执行
  */
-export const sleep = (durationMS: number): Promise<void> =>
-  durationMS <= 0
-    ? Promise.resolve(void 0)
-    : new Promise((resolve) => {
-        setTimeout(resolve, durationMS);
-      });
+export const sleep = (durationMS: number): Promise<void> => {
+  return durationMS <= 0 ? Promise.resolve(void 0) : timers.setTimeout(durationMS);
+};
