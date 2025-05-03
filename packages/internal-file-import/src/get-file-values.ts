c989625e0d2e02fe1a6e144bd3706a55ce24a2b9
geekact <geekacting@gmail.com>
2025-05-03 21:46:34 +0800

    fix(file-import): 文件路径转换成url协议

--- packages/internal-file-import/src/get-file-values.ts
+++ packages/internal-file-import/src/get-file-values.ts
@@ -1,3 +1,5 @@
+import { pathToFileURL } from 'node:url';
+
 export const getFileValues = async <T extends unknown>(
   files: string[],
   filterValues?: (item?: unknown) => boolean,
@@ -5,7 +7,8 @@ export const getFileValues = async <T extends unknown>(
   const totalFiles = new Set<T>();
   await Promise.all(
     files.map(async (file) => {
-      const modules = await import(file);
+      // Error [ERR_UNSUPPORTED_ESM_URL_SCHEME]: Only URLs with a scheme in: file, data, and node are supported by the default ESM loader. On Windows, absolute paths must be valid file:// URLs. Received protocol 'c:'
+      const modules = await import(pathToFileURL(file).toString());
       if (typeof modules === 'object') {
         Object.values(modules).forEach((item) => {
           totalFiles.add(item as any);
