3f6607c742b9bf0449cf7b657c37a3c44f290f7b
geekact <geekacting@gmail.com>
2025-07-15 23:59:06 +0800

    refactor: 更新vitest配置

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    workspace: ['packages/*'],
+    projects: ['packages/*'],
     coverage: {
       provider: 'v8',
       enabled: true,
